26552bb2638ae767e84e406f4fd4ba90aea4b9ee
Dmitry Silantiev <sylantiev21@gmail.com>
2025-09-27 09:32:00 +0300

    Создана главная страница

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import Navbar from '../components/Navbar';
+
+function MainPage() {
+    return (
+        <Box>
+            <Navbar/>
+        </Box>
+    );
+}
+
+export default MainPage;
