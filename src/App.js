38c71d27d10253595643c4addef1a1cd090da6c4
Dmitry Silantiev <sylantiev21@gmail.com>
2025-11-08 02:48:23 +0300

    Added app navigation with router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import MainPage from './pages/MainPage';
+import ProfilePage from './pages/ProfilePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <MainPage/>
+    <AuthProvider>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
