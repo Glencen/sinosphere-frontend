5b66069c0a15eec83dfd80ac7fa29e3e26aaa6e7
Dmitry Silantiev <sylantiev21@gmail.com>
2025-11-08 03:13:30 +0300

    Updated routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainPage from './pages/MainPage';
 import ProfilePage from './pages/ProfilePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage'
+import SearchResultsPage from './pages/SearchResultsPage';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/search" element={<SearchResultsPage />} />
         </Routes>
       </Router>
     </AuthProvider>
